f10e5f8f5325c0d9262b70ca4532f493baaf04ee
Perawat007 <96761242+Perawat007@users.noreply.github.com>
2023-12-27 21:05:45 +0700

    Giop

--- controllers/GameAmbApisu.js
+++ controllers/GameAmbApisu.js
@@ -1724,7 +1724,7 @@ http: exports.GameVoidBets = async (req, res) => {
                 const balanceUser = parseFloat(results[0].credit);
                 const betPlay = txnsGame[0].betAmount;
                 const betpayoutAmount = txnsGame[0].payoutAmount;
-                const balanceNow = (balanceUser - betPlay) + betpayoutAmount;
+                const balanceNow = balanceUser - betPlay;
                 const sql_update = `UPDATE member set credit='${balanceNow}',bet_latest='${betPlay}' WHERE phonenumber ='${usernameGame}'`;
                 connection.query(sql_update, (error, resultsGame) => {
                     if (error) {
